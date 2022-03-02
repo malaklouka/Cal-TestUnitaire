040108c1a1def18575c2b2f954b1bbda4c3985a3
malak <malaknakaa@gmail.com>
2022-03-02 16:28:14 +0100

    test unitaire

---
+++ src/app/calculator/calculator.component.spec.ts
@@ -0,0 +1,75 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { CalculatorComponent } from './calculator.component';
+
+describe('CalculatorComponent', () => {
+  let component: CalculatorComponent;
+  let fixture: ComponentFixture<CalculatorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CalculatorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CalculatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('deve garantir que 3 + 2 = 5', () => {
+    let btn3 = fixture.debugElement.query(By.css("#btn3"));
+    let btnSoma = fixture.debugElement.query(By.css("#btnSoma"));
+    let btn2 = fixture.debugElement.query(By.css("#btn2"));
+    let btnCalcular = fixture.debugElement.query(By.css("#btnCalcular"));
+    let display = fixture.debugElement.query(By.css("#display"));
+
+    btn3.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnSoma.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btn2.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnCalcular.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    display.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('5');
+  });
+
+  it('deve garantir que 3 - 4 = -1', () => {
+    let btn3 = fixture.debugElement.query(By.css("#btn3"));
+    let btnSubtracao = fixture.debugElement.query(By.css("#btnSubtracao"));
+    let btn4 = fixture.debugElement.query(By.css("#btn4"));
+    let btnCalcular = fixture.debugElement.query(By.css("#btnCalcular"));
+    let display = fixture.debugElement.query(By.css("#display"));
+
+    btn3.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnSubtracao.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btn4.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnCalcular.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    display.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('-1');
+  });
+});
---
+++ src/app/services/calculator.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalculatorService {
+
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
+  constructor() { }
+
+  calcular(num1: number, num2: number, operacao: string):number{
+    let result: number;
+
+    switch(operacao){
+      case(CalculatorService.SOMA):
+        result = num1 + num2;
+      break;
+      case(CalculatorService.SUBTRACAO):
+        result = num1 - num2;
+      break;
+      case(CalculatorService.DIVISAO):
+        result = num1 / num2;
+      break;
+      case(CalculatorService.MULTIPLICACAO):
+        result = num1 * num2;
+      break;
+      default:
+        result = 0;
+    }
+
+    return result;
+  }}
